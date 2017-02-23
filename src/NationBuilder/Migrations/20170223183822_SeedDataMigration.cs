975b46eca574d21880a720d5addb7b1a0b391f85
Erik Killops and Levi Bibo <erik+levi@>
2017-02-23 10:51:48 -0800

    add SeedDataMigration to seed database with events, structures, and government

---
+++ src/NationBuilder/Migrations/20170223183822_SeedDataMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NationBuilder.Models;
+
+namespace NationBuilder.Migrations
+{
+    public partial class SeedDataMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (NationBuilderDbContext db = new NationBuilderDbContext())
+            {
+                db.Governments.AddRange(SeedData.SeedGovernments);
+                db.Structures.AddRange(SeedData.SeedStructures);
+                db.Events.AddRange(SeedData.SeedEvents);
+                db.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            using (NationBuilderDbContext db = new NationBuilderDbContext())
+            {
+                db.Governments.RemoveRange(db.Governments);
+                db.Structures.RemoveRange(db.Structures);
+                db.Events.RemoveRange(db.Events);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ src/NationBuilder/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using NationBuilder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NationBuilder.Migrations
+{
+    public class SeedData
+    {
+        public static List<Government> SeedGovernments = new List<Government>()
+        {
+            new Government()
+            {
+                Name = "Dictatorship",
+                Description = "Old reliable governmental body.",
+                EconomyModifier = 1,
+                ResourcesModifier = 1,
+                PopulationModifier = -1,
+                ApprovalModifier = -1
+            },
+            new Government()
+            {
+                Name = "Theocracy",
+                Description = "Faith-based governance.",
+                EconomyModifier = -1,
+                ResourcesModifier = -1,
+                PopulationModifier = 1,
+                ApprovalModifier = 1
+            }
+        };
+
+        public static List<Structure> SeedStructures = new List<Structure>()
+        {
+            new Structure()
+            {
+                Name = "Propaganda Center",
+                Description = "Make your people think they're happy.",
+                EconomyModifier = 0,
+                ResourcesModifier = 0,
+                PopulationModifier = 0,
+                ApprovalModifier = 1
+            }
+        };
+
+        public static List<Event> SeedEvents = new List<Event>()
+        {
+            new Event()
+            {
+                Name = "False Flag",
+                Description = "Your people need to know why they need you.",
+                EconomyModifier = 0,
+                ResourcesModifier = -1,
+                PopulationModifier = -1,
+                ApprovalModifier = 1
+            }
+        };
+    }
+}
